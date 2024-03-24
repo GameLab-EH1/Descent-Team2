8d328408be708e96b2820d8e5238ca89faa4d22d
andrea-cantatore <andreacantatore01@gmail.com>
2024-03-24 22:27:29 +0100

    Setting Up CutScene

---
+++ Assets/Scripts/Managers/Story/Animation/TalkingAnimation.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalkingAnimation : MonoBehaviour
+{
+    [SerializeField] private GameObject _frame, _frame2;
+    [SerializeField] private float _animationSpeed;
+    private float _timer;
+
+}
--- Assets/Scripts/Managers/Story/DialogueManager.cs
+++ Assets/Scripts/Managers/Story/DialogueManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -5,7 +6,66 @@ using UnityEngine;
 
 public class DialogueManager : MonoBehaviour
 {
-    public TextMeshProUGUI _textComponent;
-    public string[] lines;
-    public float
+    [SerializeField] private TextMeshProUGUI _textComponent;
+    [SerializeField] private string[] _lines;
+    [SerializeField] private float _textSpeed;
+
+    private bool _isControllerPressed;
+
+    private int index;
+
+    private void Start()
+    {
+        _textComponent.text = String.Empty;
+        StartDialogue();
+    }
+    private void Update()
+    {
+        for (int i = 0;i < 20; i++) 
+        {
+            if(Input.GetKeyDown("joystick 1 button "+i))
+            {
+                _isControllerPressed = true;
+            }
+        }
+        if (Input.anyKeyDown || _isControllerPressed)
+        {
+            if (_textComponent.text == _lines[index])
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                _textComponent.text = _lines[index];
+            }
+        }
+    }
+    private void StartDialogue()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach (Char c in _lines[index].ToCharArray())
+        {
+            _textComponent.text += c;
+            yield return new WaitForSeconds(_textSpeed);
+        }
+    }
+    private void NextLine()
+    {
+        if (index < _lines.Length - 1)
+        {
+            index++;
+            _textComponent.text =String.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
 }
