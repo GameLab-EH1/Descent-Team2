6ed764f8630c4fbe88bab8a6e49afd3ae3d1ca69
andrea-cantatore <andreacantatore01@gmail.com>
2024-03-24 21:56:07 +0100

    Cutscene

---
+++ Assets/Scripts/Managers/Story/DialogueManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    public TextMeshProUGUI _textComponent;
+    public string[] lines;
+    public float
+}
